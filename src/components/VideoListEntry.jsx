7a58202e9341222670bc4331dbd3f948b0c45afe
Katherine Wang <katherine.wang429@gmail.com>
2019-11-17 21:42:54 -0800

    Fix thumbnail rendering

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,7 +1,7 @@
 var VideoListEntry = ({ video, index }) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src={`https://i.ytimg.com/vi/${video.id.videoId}/default.jpg`} alt="" />
+      <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">{video.snippet.title}</div>
