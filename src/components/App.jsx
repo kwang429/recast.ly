f5ac6b8d24d433e41905e8e5f891678006b66467
Katherine Wang <katherine.wang429@gmail.com>
2019-11-17 21:40:01 -0800

    Refactor App.jsx to be a stateful component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,35 @@ import Search from './Search.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 
-var App = ({ videos }) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em><Search /></em></h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: props.videos,
+      currentVideo: props.videos[0]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em><Search /></em></h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em><VideoPlayer video={this.state.currentVideo} /></em></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em><VideoList videos={this.state.videos} /></em></h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em><VideoPlayer video={videos[0]}/></em></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em><VideoList videos={videos}/></em></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
