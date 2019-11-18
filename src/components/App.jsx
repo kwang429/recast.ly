1042be42c55e77310fb095ed4ac1b5473cb5fdd4
Katherine Wang <katherine.wang429@gmail.com>
2019-11-17 21:31:17 -0800

    Solo redo - render everything in stateless components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-var App = () => (
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
+var App = ({ videos }) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em><Search /></em></h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em><VideoPlayer video={videos[0]}/></em></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em><VideoList videos={videos}/></em></h5></div>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import ExampleVideoData from '../src/data/exampleVideoData.js';
 
+ReactDOM.render(<App videos={ExampleVideoData}/>, document.getElementById('app'));
